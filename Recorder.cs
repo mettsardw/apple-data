9bbe971027f3a73831823a82a700923990059bbb
mettsardw <30555201+mettsardw@users.noreply.github.com>
2017-07-29 23:53:46 +0700

    Mic script for AR
    Suitable for both built-in mic or headset mic.

---
+++ Recorder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recorder : MonoBehaviour {
+
+	public static string DeviceName;
+
+	// Use this for initialization
+	void Start () {
+		foreach (string device in Microphone.devices) {
+			DeviceName = device;
+			Debug.Log("Name: " + DeviceName);
+		}
+
+		AudioSource aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start(DeviceName, true, 100, 44100);
+		aud.Play();
+
+		if (Microphone.IsRecording (DeviceName)) {
+			Debug.Log ("Recording...");
+		}
+
+		//aud.clip = Microphone.End ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
